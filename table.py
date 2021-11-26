1175c25a4e3826db2ae161fa81c61ae61cb25a11
podlaw <36234864+podlaw@users.noreply.github.com>
2021-11-26 18:42:08 +0300

    Add files via upload

---
+++ table.py
@@ -0,0 +1,12 @@
+def table(n: int, m: int, r: int, firstRow: list) -> list:
+    '''Находит последнюю строку таблицы размера nxm
+    :param
+    n - количество строк
+    m - количество столбов
+    r - модуль, по которому выполняются вычисления
+    firstRow - первая строка таблицы
+    lastRow - последняя строка таблицы'''
+    
+    lastRow=firstRow
+    return lastRow
+    
